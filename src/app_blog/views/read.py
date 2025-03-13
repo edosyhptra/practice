4f2fa29dd9b318050a05b0e9b00a357ceea7a4c0
edosyhptra <edo23syahputra@gmail.com>
2025-03-13 20:59:09 +0700

    add read post feature

--- src/app_blog/views/read.py
+++ src/app_blog/views/read.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from app_blog.utility import query
 
 def view(request, id):
-    context = {
-        'id': id
-    }
+    if request.method == 'GET':
+        result = query("SELECT * FROM blog_post WHERE id = %s", [id])
+        if not result:
+            return render(request, "app_blog/read.html", {"error": "Post tidak ditemukan"})
+        
+        print(result)
+    return render(request, "app_blog/read.html", {"post": result[0]})
 
-    return render(request, 'app_blog/read.html', context)
+    
