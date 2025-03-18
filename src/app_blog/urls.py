1b658bc166541162d7cdb197bb3a2d49738f67a4
edosyhptra <edo23syahputra@gmail.com>
2025-03-18 10:50:47 +0700

    add list_post feature

--- src/app_blog/urls.py
+++ src/app_blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import read, create
+from .views import read, create, list
 
 urlpatterns = [
     path('', create.view, name='blog-create'),
     path('<int:id>/', read.view, name='blog-read'),
+    path('list/', list.view, name='blog-list'),
 ]
